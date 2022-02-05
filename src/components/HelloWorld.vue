617d07515552eb9b03e30c1af26cb474fe20bb1a
Levi <levi@192.168.1.8>
2022-02-05 17:40:28 +0700

    initial quasar

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,160 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="flex flex-center">
+    <div >
+      <div class="text-center q-mt-md">
+        <button color="primary" class="full-width" @click="print()">Print</button>
+      </div>
+    </div>
   </div>
 </template>
 
+<style>
+</style>
+
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  name: 'PageIndex',
+  data () {
+    return {
+      msg: 'dsadsad',
+      printCharacteristic: null,
+      // isMobile: this.$q.platform.is.mobile
+    }
+  },
+  computed: {
+    userAgentType(){
+        const matchType = [
+            /Android/i,
+            /webOS/i,
+            /iPhone/i,
+            /iPad/i,
+            /iPod/i,
+            /BlackBerry/i,
+            /Windows Phone/i
+        ]
+
+        return matchType.some((matchItem) => {
+            return navigator.userAgent.match(matchItem)
+        })
+    },
+  },
+  methods: {
+    print () {
+      navigator.bluetooth
+        .requestDevice(
+          {
+            filters: [
+              {
+                name: 'BlueTooth Printer',
+                services: ['000018f0-0000-1000-8000-00805f9b34fb']
+              }
+            ]
+          }
+        )
+        .then(device => {
+          console.log('device', device)
+
+          if (device.gatt.connected) {
+            device.gatt.disconnect()
+          }
+          
+          console.log('connect')
+          return this.connect(device)
+        })
+        .catch(this.handleError)
+    },
+    connect (device) {
+      const self = this
+      device.addEventListener('gattserverdisconnected', this.onDisconnected)
+      return device.gatt
+        .connect()
+        .then(server =>
+          server.getPrimaryService('000018f0-0000-1000-8000-00805f9b34fb')
+        )
+        .then(service =>
+          service.getCharacteristic('00002af1-0000-1000-8000-00805f9b34fb')
+        )
+        .then(characteristic => {
+          console.log('characteristic', characteristic)
+          self.printCharacteristic = characteristic
+          self.sendTextData(device)
+        })
+        .catch(error => {
+          this.handleError(error, device)
+        })
+    },
+    handleError (error, device) {
+      console.error('handleError => error', error)
+      if (device != null) {
+        device.gatt.disconnect()
+      }
+      let erro = JSON.stringify({
+        code: error.code,
+        message: error.message,
+        name: error.name
+      })
+
+      console.log('handleError => erro', erro)
+      if (error.code !== 8) {
+        alert('Could not connect with the printer. Try it again')
+      }
+    },
+    getBytes (text) {
+      console.log('text', text)
+      let br = '\u000A\u000D'
+      text = text === undefined ? br : text
+      let replaced = this.$languages.replace(text)
+      console.log('replaced', replaced)
+      let bytes = new TextEncoder('utf-8').encode(replaced + br)
+      console.log('bytes', bytes)
+      return bytes
+    },
+    addText (arrayText) {
+      let text = this.msg
+      arrayText.push(text)
+      if (this.userAgentType) {
+        while (text.length >= 20) {
+          let text2 = text.substring(20)
+          arrayText.push(text2)
+          text = text2
+        }
+      }
+    },
+    sendTextData (device) {
+      let arrayText = []
+      this.addText(arrayText)
+      console.log('sendTextData => arrayText', arrayText)
+      this.loop(0, arrayText, device)
+    },
+    loop (i, arrayText, device) {
+      let arrayBytes = this.getBytes(arrayText[i])
+      this.write(device, arrayBytes, () => {
+        i++
+        if (i < arrayText.length) {
+          this.loop(i, arrayText, device)
+        } else {
+          let arrayBytes = this.getBytes()
+          this.write(device, arrayBytes, () => {
+            device.gatt.disconnect()
+          })
+        }
+      })
+    },
+    write (device, array, callback) {
+      this.printCharacteristic
+        .writeValue(array)
+        .then(() => {
+          console.log('Printed Array: ' + array.length)
+          setTimeout(() => {
+            if (callback) {
+              callback()
+            }
+          }, 250)
+        })
+        .catch(error => {
+          this.handleError(error, device)
+        })
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
